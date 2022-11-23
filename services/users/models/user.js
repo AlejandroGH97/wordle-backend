79d2e84c26abf62816f88eb2a18e88bba7fa1f17
alejandrogh97 <alejandrogoicochea@gmail.com>
2022-11-23 11:04:30 -0500

    feat(user): signup and login

--- services/users/models/user.js
+++ services/users/models/user.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const wordSchema = new Schema({
-  value: { type: String, required: true },
-  description: { type: String, required: true },
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
   statistics: {
-    1: { type: Number, required: true },
-    2: { type: Number, required: true },
-    3: { type: Number, required: true },
-    4: { type: Number, required: true },
-    5: { type: Number, required: true },
-    6: { type: Number, required: true },
-    fail: { type: Number, required: true },
+    1: { type: Number, required: true, default: 0 },
+    2: { type: Number, required: true, default: 0 },
+    3: { type: Number, required: true, default: 0 },
+    4: { type: Number, required: true, default: 0 },
+    5: { type: Number, required: true, default: 0 },
+    6: { type: Number, required: true, default: 0 },
+    fail: { type: Number, required: true, default: 0 },
   },
   pickDate: { type: Date },
 });
 
-module.exports = mongoose.model('Word', wordSchema);
+module.exports = mongoose.model('User', userSchema);
