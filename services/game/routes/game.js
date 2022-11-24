0ca75dfe51454bef82c1e3d677f214ae13f85f62
alejandrogh97 <alejandrogoicochea@gmail.com>
2022-11-23 23:55:41 -0500

    feat(game): update word statistics

--- services/game/routes/game.js
+++ services/game/routes/game.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { authenticateToken } = require('@wordle/auth-middleware');
 const word = require('../models/word');
 
 const router = express.Router();
@@ -9,21 +10,21 @@ router.get('/init', async (req, res, next) => {
     date.setDate(date.getUTCDate());
     dateFilter = new Date(date.setHours(0, 0, 0, 0));
 
-    const [{ value: todaysWord }, [{ values: dictionary }]] = await Promise.all(
-      [
-        word.findOne({}, { _id: 0, value: 1 }).sort({ pickDate: -1 }),
-        word.aggregate([
-          {
-            $group: {
-              _id: null,
-              values: {
-                $push: '$value',
-              },
+    const [todaysWord, [{ values: dictionary }]] = await Promise.all([
+      word
+        .findOne({}, { _id: 0, value: 1, description: 1 })
+        .sort({ pickDate: -1 }),
+      word.aggregate([
+        {
+          $group: {
+            _id: null,
+            values: {
+              $push: '$value',
             },
           },
-        ]),
-      ],
-    );
+        },
+      ]),
+    ]);
 
     res.json({
       todaysWord,
@@ -32,6 +33,21 @@ router.get('/init', async (req, res, next) => {
   } catch (error) {
     res.status(418).send(error);
   }
+});
+
+router.post('/finish', authenticateToken, async (req, res, next) => {
+  const { value: wordValue, tries } = req.body;
+  const updateKey = `statistics.${tries}`;
+  const updatedWord = await word.findOneAndUpdate(
+    { value: wordValue },
+    {
+      $inc: {
+        [updateKey]: 1,
+      },
+    },
+    { returnDocument: 'after' },
+  );
+  res.send(updatedWord.statistics);
 });
 
 module.exports = router;
